29752d1474ce4662951556e53980e7ea98236767
shukikat <kathuriashuki@gmail.com>
2024-11-04 17:44:18 -0500

    initial seed data

---
+++ seeders/20241104123406-seed-filename.js
@@ -0,0 +1,29 @@
+
+
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('User', [{
+     *   email: 'test@test.com',
+     *   password: 'password',
+     *   location: 'New York',
+     *   time_zone: 
+     * }], {});
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
