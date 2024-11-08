0c69429d874f21321063653b00145d9f89d4215d
ShannonMG <shannonmastrogiovanni@gmail.com>
2024-11-07 19:59:34 -0500

    same thign

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import styles from './Header.module.css';
+import styles from './header.module.css';
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
